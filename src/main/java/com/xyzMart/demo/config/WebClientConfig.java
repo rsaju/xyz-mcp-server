12f5455e40da53ffbb323a03f745fef341665c4e
Roshan <sajuroshan05@gmail.com>
2026-08-15 11:44:58 +1000

    Added Oauth2 security to make inventory api call

--- src/main/java/com/xyzMart/demo/config/WebClientConfig.java
+++ src/main/java/com/xyzMart/demo/config/WebClientConfig.java
@@ -3,6 +3,8 @@ package com.xyzMart.demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -12,8 +14,12 @@ public class WebClientConfig {
     String inventoryBaseUrl;
 
     @Bean
-    public WebClient inventoryWebClient(){
+    public WebClient inventoryWebClient(OAuth2AuthorizedClientManager authorizedClientManager){
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Client.setDefaultClientRegistrationId("inventory-service-client");
         return WebClient.builder()
-                .baseUrl(inventoryBaseUrl).build();
+                .baseUrl(inventoryBaseUrl)
+                .apply(oauth2Client.oauth2Configuration())
+                .build();
     }
 }
